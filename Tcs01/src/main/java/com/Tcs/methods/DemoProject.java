5e6cff9b043270e0af8411a249187fcd91338028
Aditya <Aditya@Laptop-Aditya>
2023-01-06 13:51:53 +0530

    By by git hub

--- Tcs01/src/main/java/com/Tcs/methods/DemoProject.java
+++ Tcs01/src/main/java/com/Tcs/methods/DemoProject.java
@@ -5,6 +5,8 @@ public class DemoProject {
 	public static void main(String[] args) {
 		
 		System.out.println("welecome o github");
+                System.out.println("Good by too github");
+
 		
 		
 	}
